47afd9f9ed23083b7862aba6ecd80e77674b8a47
HDodge <herbiedodge@gmail.com>
2020-09-12 10:31:37 -0500

    ToDoListVC setup

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Herbert Dodge on 9/12/20.
+//  Copyright © 2020 Herbert Dodge. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    //MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+}
+
