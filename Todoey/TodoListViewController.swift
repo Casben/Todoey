2045abb0363418e59d725d142c4a5555a1cf654e
HDodge <herbiedodge@gmail.com>
2020-09-13 22:11:27 -0500

    added barbutton item and functionality

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = [String]()
+    var itemArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    //MARK: - Methods
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
 
     //MARK: - TableView Datasource Methods
     
